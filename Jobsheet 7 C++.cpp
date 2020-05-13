52de0518044a79edc84331f505cc52484bd1639a
Ainul Hikam <64570406+Ainulhikam@users.noreply.github.com>
2020-05-13 14:16:36 -0300

    Create Jobsheet 7 C++.cpp
    Mengisi Elemen Array menggunakan perulangan

---
+++ Jobsheet 7 C++.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+main (){
+	cout <<"Nama : Ainul Hikam"<<endl;
+	cout <<"Nim : F1B019017"<<endl;
+	cout <<"Kelompok : 5"<<endl;
+int nim [7];
+for (int i = 0; i<= 7; i++){
+cout<< "Masukkan nim ke- " << i+1;
+cin>>nim[i];
+}
+}
