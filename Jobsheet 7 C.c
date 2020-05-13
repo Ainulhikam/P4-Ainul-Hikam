df22bcb4c597381fd674b3d58e32d2f9d3bf7244
Ainul Hikam <64570406+Ainulhikam@users.noreply.github.com>
2020-05-13 14:17:42 -0300

    Create Jobsheet 7 C.c
    Mengisi Elemen Array menggunakan perulangan

---
+++ Jobsheet 7 C.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+main (){
+	printf ("Nama : Ainul Hikam \n");
+	printf ("Nim : F1B019017 \n");
+	printf ("Kelompok : 5 \n");
+int nim [7], i;
+for (i = 0; i<= 7; i++){
+printf ("Masukkan nim ke-%i ", i+1);
+scanf ("%i", &nim[i]);
+}
+}
