24912ce19fa1c56daa139567765718bcd1477ef5
Ainul Hikam <64570406+Ainulhikam@users.noreply.github.com>
2020-05-13 13:59:28 -0300

    Create Jobsheet 2 C++.cpp
    Mengisi dan menampilkan elemen array (dinamis)

---
+++ Jobsheet 2 C++.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main (){
+
+cout <<"Nama : Ainul Hikam"<<endl;
+cout <<"Nim : F1B019017"<<endl;
+cout <<"Kelomok :5"<<endl;
+cout <<endl;
+
+int x [7];
+
+cout <<"masukan nim 1 : ";
+cin  >>x[0];
+cout <<"masukan nim 2 : ";
+cin  >>x[1];
+cout <<"masukan nim 3 : ";
+cin  >>x[2];
+cout <<"masukan nim 4 : ";
+cin  >>x[3];
+cout <<"masukan nim 5 : ";
+cin  >>x[4];
+cout <<"masukan nim 6 : ";
+cin  >>x[5];
+cout <<"masukan nim 7 : ";
+cin  >>x[6];
+return 0 ;}
